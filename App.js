e3efa73bedd62515d04ec1b444eff8805acc568f
KondaDivyaswari <141744946+KondaDivyaswari@users.noreply.github.com>
2023-09-25 13:24:39 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import ArrayOfObjs from './ArrayOfObjs';
+
+function App() {
+  return (
+    <div className="App">
+         <ArrayOfObjs/>
+    </div>
+  );
+}
+
+export default App;
